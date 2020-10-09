6fb65899aae6c5e8eb5942df8495837201f05143
Sohail Memon <52044897+SohailTSM@users.noreply.github.com>
2020-10-10 02:35:41 +0530

    Update testing_nltk_2.py

--- testing_nltk_2.py
+++ testing_nltk_2.py
@@ -2,8 +2,8 @@ import nltk
 from nltk.tag import tnt
 from nltk.corpus import indian
 
-text = "रामलाल ह अपन मकान के ढलई करत रिहिस"
-train_data = indian.tagged_sents('/cg_tagged.txt')
+text = "हमन राजिम मेला गे रहेन"
+train_data = indian.tagged_sents('/home/tsm/Minor Project/cg_tagged.txt')
 tnt_pos_tagger = tnt.TnT()
 tnt_pos_tagger.train(train_data)
 tagged_words = (tnt_pos_tagger.tag(nltk.word_tokenize(text)))
@@ -13,9 +13,80 @@ tokens = list(text.split(' '))
 tagged_data = {
     tagged_words[0][1]: [[tagged_words[0][0]]]
 }
+
 for i in range(1, len(tagged_words)):
     if tagged_words[i][1] in tagged_data:
         tagged_data[tagged_words[i][1]].append([tagged_words[i][0]])
     else:
         tagged_data[tagged_words[i][1]] = [[tagged_words[i][0]]]
-print(tagged_data)
+
+r = {
+    'S': [["NP", "VP"]],
+    'NP': [np],
+    'VP': [vp],
+}
+r.update(tagged_data)
+
+
+def toCNF(p, key):
+    ret = []
+    while len(p) > 2:
+        ret.append([p[0], key])
+        p.pop(0)
+    ret.append(p)
+    return ret
+
+
+r['VP'] = toCNF(vp, 'VP')
+r['NP'] = toCNF(np, 'NP')
+
+
+def cykParse(w):
+    n = len(w)
+
+    # Initialize the table
+    T = [[set([]) for j in range(n)] for i in range(n)]
+
+    # Filling in the table
+    for j in range(0, n):
+
+        # Iterate over the rules
+        for lhs, rule in r.items():
+            for rhs in rule:
+
+                # If a terminal is found
+                if len(rhs) == 1 and \
+                        rhs[0] == w[j]:
+                    T[j][j].add(lhs)
+
+        for i in range(j, -1, -1):
+
+            # Iterate over the range i to j + 1
+            for k in range(i, j + 1):
+
+                # Iterate over the rules
+                for lhs, rule in r.items():
+                    for rhs in rule:
+
+                        # If a terminal is found
+                        if len(rhs) == 2 and rhs[0] in T[i][k] and rhs[1] in T[k + 1][j]:
+                            T[i][j].add(lhs)
+
+    # If word can be formed by rules
+    # of given grammar
+
+    for i in range(n):
+        for j in range(n):
+            print(T[i][j], end=" ")
+        print()
+
+    if len(T[0][n-1]) != 0:
+        print("\nYes, the given sentence belongs to CFG")
+    else:
+        print("\nNo, the given sentence does not belongs to CFG")
+
+
+print("\n{}\n".format(text))
+print("\n{}\n".format(tagged_words))
+print("\n{}\n\n".format(r))
+cykParse(tokens)
