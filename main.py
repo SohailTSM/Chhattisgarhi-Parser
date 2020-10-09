9ac8f05b3ed2e7eca354b322ad76398424c3c64d
Srijan Sharma <srijans38@gmail.com>
2020-10-10 03:29:02 +0530

    API

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
 import nltk
+import pathlib
 from nltk.tag import tnt
 from nltk.corpus import indian
-
-text = "हमन राजिम मेला गे रहेन"
-train_data = indian.tagged_sents('/home/tsm/Minor Project/cg_tagged.txt')
-tnt_pos_tagger = tnt.TnT()
-tnt_pos_tagger.train(train_data)
-tagged_words = (tnt_pos_tagger.tag(nltk.word_tokenize(text)))
-tags = list(map(lambda x: x[1], tagged_words))
-np, vp = tags[: tags.index('VM')], tags[tags.index('VM'):]
-tokens = list(text.split(' '))
-tagged_data = {
-    tagged_words[0][1]: [[tagged_words[0][0]]]
-}
-
-for i in range(1, len(tagged_words)):
-    if tagged_words[i][1] in tagged_data:
-        tagged_data[tagged_words[i][1]].append([tagged_words[i][0]])
-    else:
-        tagged_data[tagged_words[i][1]] = [[tagged_words[i][0]]]
-
-r = {
-    'S': [["NP", "VP"]],
-    'NP': [np],
-    'VP': [vp],
-}
-r.update(tagged_data)
-
 
 def toCNF(p, key):
     ret = []
@@ -36,12 +11,7 @@ def toCNF(p, key):
     ret.append(p)
     return ret
 
-
-r['VP'] = toCNF(vp, 'VP')
-r['NP'] = toCNF(np, 'NP')
-
-
-def cykParse(w):
+def cykParse(w, r):
     n = len(w)
 
     # Initialize the table
@@ -82,11 +52,42 @@ def cykParse(w):
 
     if len(T[0][n-1]) != 0:
         print("\nYes, the given sentence belongs to CFG")
+        return r
     else:
         print("\nNo, the given sentence does not belongs to CFG")
+        return "cykError"
 
+text = "हमन राजिम मेला गे रहेन"
 
-print("\n{}\n".format(text))
-print("\n{}\n".format(tagged_words))
-print("\n{}\n\n".format(r))
-cykParse(tokens)
+def main(text):
+    try:
+        path = str(pathlib.Path(__file__).parent.absolute())
+        train_data = indian.tagged_sents(path + '/cg_tagged.txt')
+        tnt_pos_tagger = tnt.TnT()
+        tnt_pos_tagger.train(train_data)
+        tagged_words = (tnt_pos_tagger.tag(nltk.word_tokenize(text)))
+        tags = list(map(lambda x: x[1], tagged_words))
+        np, vp = tags[: tags.index('VM')], tags[tags.index('VM'):]
+        tokens = list(text.split(' '))
+        tagged_data = {
+            tagged_words[0][1]: [[tagged_words[0][0]]]
+        }
+
+        for i in range(1, len(tagged_words)):
+            if tagged_words[i][1] in tagged_data:
+                tagged_data[tagged_words[i][1]].append([tagged_words[i][0]])
+            else:
+                tagged_data[tagged_words[i][1]] = [[tagged_words[i][0]]]
+
+        r = {
+            'S': [["NP", "VP"]],
+            'NP': [np],
+            'VP': [vp],
+        }
+        r.update(tagged_data)
+        r['VP'] = toCNF(vp, 'VP')
+        r['NP'] = toCNF(np, 'NP')
+        return cykParse(tokens, r)
+    except:
+        return "textError"
+    
---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import main
+
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST', 'GET'])
+def api():
+    if request.method == 'POST':
+        text = request.form['text']
+        response = main.main(text)
+        
+        return {
+            'status' : 1 if type(response) is dict else 0,
+            'response' : response
+        }
+
+    return {
+        'error' : 'No text input. Use POST method'
+    }
